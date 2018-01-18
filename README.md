7852bf6f2d7dce91825095ded5394af853a5caa6
David Ramiro <david@kore.cc>
2018-01-18 20:09:42 +0100

    rework config and add temperature range
    Reworked the config file and parsing. The user may now add the color temperature range of his lamps.

--- README.md
+++ README.md
@@ -8,14 +8,15 @@ Setting up a light scene with Yeelight smart lamps corresponding to your f.lux d
 
 ## Usage
 * Enable the [developer mode](https://www.yeelight.com/en_US/developer) (sometimes also called LAN mode) on your lamps in the Yeelight app.
-* Find out your lamp's IP addresses and put them into the config.ini file aswell as the number of lamps you want to control.
+* Find out your lamp's IP addresses and put them into the config.ini file.
+* If you know your lamp's color temperature range you should set it in the config file aswell. If you don't know it, the script will use the widest range possible but you might get an error message. It will still work, though.
 * In f.lux settings enter `http://127.0.0.1:8080/room_1` in the bottom URL mask as shown [here](https://i.imgur.com/ybEWdIC.png).
 * Run fluxee.py and enjoy.
 
 
 ## Important note
 
-Keep in mind that your f.lux settings might not match your lamp's capabilities. My Yeelight RGBW Bulb supports 1700K to 6500K, my Mi Desk Lamp only 2700K to 6500K. f.lux ranges from 1900K to 6500K. Your mileage may vary depending on your lamp, if you're very sensitive even 2700K might not be dim enough for you to achieve full melatonin secretion.
+Keep in mind that your f.lux settings might not match your lamp's capabilities. For example, my Yeelight RGBW Bulb supports 1700K to 6500K, my Mi Desk Lamp only 2700K to 6500K. On default, f.lux ranges from 1900K to 6500K, its effectiveness may vary depending on your lamp. If you're very sensitive even 2700K might not be dim enough for you to achieve full melatonin secretion.
 
 ## Credits
 Gratefully using the Yeelight python library by Stavros Korokithakis.
--- fluxee.py
+++ fluxee.py
@@ -9,53 +9,142 @@ def room_handler():
         ct = int(post_dict['ct'])
         bri = int(float(post_dict['bri']) * 100)
         
-        if lamps >= '1':
+        if bulb1 != '':
             print('Setting first lamp...')
             bulb = Bulb(bulb1)
-            bulb.set_color_temp(ct)
-            print('Color temperature set to %s' % ct)
             bulb.set_brightness(bri)
-            print('Brightness set to %s' % (bri))
+            print('Brightness set to %s percent' % (bri))
+            if mintemp1 < ct < maxtemp1:
+                bulb.set_color_temp(ct)
+                print('Color temperature set to %s Kelvin' % ct)
+            else:
+                if ct > maxtemp1:
+                    bulb.set_color_temp(maxtemp1)
+                    print('Reached highest color temperature of %s Kelvin' % maxtemp1)
+                if ct < mintemp1:
+                    bulb.set_color_temp(mintemp1)
+                    print('Reached lowest color temperature of %s Kelvin' % mintemp1)
         
-        if lamps >= '2':
+        if bulb2 != '':
             print('Setting second lamp...')
             bulb = Bulb(bulb2)
-            bulb.set_color_temp(ct)
-            print('Color temperature set to %s' % ct)
             bulb.set_brightness(bri)
-            print('Brightness set to %s' % (bri))
+            print('Brightness set to %s percent' % (bri))
+            if mintemp2 < ct < maxtemp2:
+                bulb.set_color_temp(ct)
+                print('Color temperature set to %s Kelvin' % ct)
+            else:
+                if ct > maxtemp2:
+                    bulb.set_color_temp(maxtemp2)
+                    print('Reached highest color temperature of %s Kelvin' % maxtemp2)
+                if ct < mintemp5:
+                    bulb.set_color_temp(mintemp2)
+                    print('Reached lowest color temperature of %s Kelvin' % mintemp2)
         
-        if lamps >= '3':
+        if bulb3 != '':
             print('Setting third lamp...')
             bulb = Bulb(bulb3)
-            bulb.set_color_temp(ct)
-            print('Color temperature set to %s' % ct)
             bulb.set_brightness(bri)
-            print('Brightness set to %s' % (bri))
+            print('Brightness set to %s percent' % (bri))
+            if mintemp3 < ct < maxtemp3:
+                bulb.set_color_temp(ct)
+                print('Color temperature set to %s Kelvin' % ct)
+            else:
+                if ct > maxtemp3:
+                    bulb.set_color_temp(maxtemp3)
+                    print('Reached highest color temperature of %s Kelvin' % maxtemp3)
+                if ct < mintemp3:
+                    bulb.set_color_temp(mintemp3)
+                    print('Reached lowest color temperature of %s Kelvin' % mintemp3)
         
-        if lamps >= '4':
+        if bulb4 != '':
             print('Setting fourth lamp...')
             bulb = Bulb(bulb4)
-            bulb.set_color_temp(ct)
-            print('Color temperature set to %s' % ct)
             bulb.set_brightness(bri)
-            print('Brightness set to %s' % (bri))
+            print('Brightness set to %s percent' % (bri))
+            if mintemp4 < ct < maxtemp4:
+                bulb.set_color_temp(ct)
+                print('Color temperature set to %s Kelvin' % ct)
+            else:
+                if ct > maxtemp4:
+                    bulb.set_color_temp(maxtemp4)
+                    print('Reached highest color temperature of %s Kelvin' % maxtemp4)
+                if ct < mintemp4:
+                    bulb.set_color_temp(mintemp4)
+                    print('Reached lowest color temperature of %s Kelvin' % mintemp4)
         
-        if lamps >= '5':
+        if bulb5 != '':
             print('Setting fifth lamp...')
             bulb = Bulb(bulb5)
-            bulb.set_color_temp(ct)
-            print('Color temperature set to %s' % ct)
             bulb.set_brightness(bri)
-            print('Brightness set to %s' % (bri))
+            print('Brightness set to %s percent' % (bri))
+            if mintemp5 < ct < maxtemp5:
+                bulb.set_color_temp(ct)
+                print('Color temperature set to %s Kelvin' % ct)
+            else:
+                if ct > maxtemp5:
+                    bulb.set_color_temp(maxtemp5)
+                    print('Reached highest color temperature of %s Kelvin' % maxtemp5)
+                if ct < mintemp5:
+                    bulb.set_color_temp(mintemp5)
+                    print('Reached lowest color temperature of %s Kelvin' % mintemp5)
+
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-bulb1 = config.get('config','ip1')
-bulb2 = config.get('config','ip2')
-bulb3 = config.get('config','ip3')
-bulb4 = config.get('config','ip3')
-bulb5 = config.get('config','ip3')
-lamps = config.get('config','lamps')
+bulb1 = config.get('lamp one','ip')
+maxtemp1 = config.get('lamp one','MaxColorTemperature')
+mintemp1 = config.get('lamp one','MinColorTemperature')
+if mintemp1 == '':
+    mintemp1 = 1700
+if maxtemp1 == '':
+    maxtemp1 = 6500
+else:
+    mintemp1 = int(mintemp1)
+    maxtemp1 = int(maxtemp1)
+
+bulb2 = config.get('lamp two','ip')
+maxtemp2 = config.get('lamp two','MaxColorTemperature')
+mintemp2 = config.get('lamp two','MinColorTemperature')
+if mintemp2 == '':
+    mintemp2 = 1700
+if maxtemp2 == '':
+    maxtemp2 = 6500
+else:
+    mintemp2 = int(mintemp2)
+    maxtemp2 = int(maxtemp2)
+
+bulb3 = config.get('lamp three','ip')
+maxtemp3 = config.get('lamp three','MaxColorTemperature')
+mintemp3 = config.get('lamp three','MinColorTemperature')
+if mintemp3 == '':
+    mintemp3 = 1700
+if maxtemp3 == '':
+    maxtemp3 = 6500
+else:
+    mintemp3 = int(mintemp3)
+    maxtemp3 = int(maxtemp3)
+
+bulb4 = config.get('lamp four','ip')
+maxtemp4 = config.get('lamp four','MaxColorTemperature')
+mintemp4 = config.get('lamp four','MinColorTemperature')
+if mintemp4 == '':
+    mintemp4 = 1700
+if maxtemp4 == '':
+    maxtemp4 = 6500
+else:
+    mintemp4 = int(mintemp4)
+    maxtemp4 = int(maxtemp4)
+
+bulb5 = config.get('lamp five','ip')
+maxtemp5 = config.get('lamp five','MaxColorTemperature')
+mintemp5 = config.get('lamp five','MinColorTemperature')
+if mintemp5 == '':
+    mintemp5 = 1700
+if maxtemp5 == '':
+    maxtemp5 = 6500
+else:
+    mintemp5 = int(mintemp5)
+    maxtemp5 = int(maxtemp5)
 
 run(host='127.0.0.1', port=8080)
