748081531477c819d1d9224befab4dc7b46cd0a6
David Ramiro <david@kore.cc>
2018-07-26 02:31:51 +0200

    Add brightness setting
    You can now set a static brightness if the default starting brightness of 100% is too much for you or if you don't like f.lux way of dynamically changing brightness.

--- fluxee.py
+++ fluxee.py
@@ -21,8 +21,9 @@ def room_handler():
             if currentbulb != '':
                 print('Sending command to Yeelight at', currentbulb)
                 bulb = Bulb(currentbulb)
-                bulb.set_brightness(bri)
-                print('Brightness set to', bri, 'percent')
+                if static_brightness is False:
+                    bulb.set_brightness(bri)
+                    print('Brightness set to', bri, 'percent')
                 if int(mintemp) < ct < int(maxtemp):
                     bulb.set_color_temp(ct)
                     print('Color temperature set to', ct, 'Kelvin')
@@ -40,7 +41,11 @@ def main():
     print('Reading config...')
     config = configparser.ConfigParser()
     config.read('config.ini')
+    global static_brightness
     bulb_count = int(config.get('general', 'LampCount'))
+    check_state = config.getboolean('general', 'CheckLampState')
+    static_brightness = config.getboolean('general', 'StaticBrightness')
+    default_brightness = int(config.get('general', 'BrightnessValue'))
     for n in range(1, (bulb_count + 1)):
         bulbs.append(config.get(str(n), 'ip'))
         if config.get(str(n), 'MaxColorTemperature') == '':
@@ -56,14 +61,20 @@ def main():
         if init_bulb != '':
             print('Initializing Yeelight at %s' % init_bulb)
             bulb = Bulb(init_bulb)
-            if check_state == True:
+            if check_state is True:
                 state = bulb.get_properties(requested_properties=['power'])
                 if 'off' in state['power']:
                     print('Powered off. Ignoring Yeelight at %s' % init_bulb)
+                elif static_brightness is True:
+                    print('Changing brightness of', init_bulb)
+                    bulb.set_brightness(default_brightness)
             else:
                 print('Turning on Yeelight at %s' % init_bulb)
                 bulb.turn_on()
-                bulb.set_brightness(100)
+                if static_brightness is True:
+                    bulb.set_brightness(default_brightness)
+                else:
+                    bulb.set_brightness(100)
 
     run(host='127.0.0.1', port=8080)
     print('Thank you for using fluxee. Have a good one!')
